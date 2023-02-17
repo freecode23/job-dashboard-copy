c541a9b474c9d1d60a125ad33d00666092367a7e
Ubuntu <ubuntu@ip-10-0-4-143.us-east-2.compute.internal>
2023-02-17 19:03:44 +0000

    dont include location code in params

--- server/util/util.js
+++ server/util/util.js
@@ -47,7 +47,6 @@ const setParams = (req) => {
         google_domain: "google.com",
         location_requested: location.country,
         location_used: location.country,
-        location_code: location.gl,
         hl: "en",
         gl: "us" // country
     }
